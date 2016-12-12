0639b6f1db36fbad6e7c76102f4579feeba6bbff
Phil <plmok61@gmail.com>
2016-12-12 15:15:28 -0600

    adds todos to an array

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-app.js
+angular.module('todoApp', [])
+  .controller('TodoListController', function() {
+    var todoList = this
+    todoList.todos = []
+
+    todoList.addTodo = function() {
+      todoList.todos.push({text: todoList.todoText, completed: false})
+      todoList.todoText = ''
+      console.log('todos:',todoList.todos)
+    }
+
+})
