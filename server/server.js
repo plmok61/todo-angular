f75437eb0ee0dc3d8569910f29ad614ef776d236
Phil <plmok61@gmail.com>
2016-12-13 15:59:58 -0600

    sets up basic express server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
-var http = require('http')
+var express = require('express')
+var app = express()
+var path = require('path')
 
-http.createServer(function(request, response) {
 
-  response.end('hello world')
-}).listen(3000)
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve(__dirname, '../client/index.html'))
+})
+
+var port = 3000
+
+app.listen(port, function () {
+  console.log('Running on port ', port)
+})
