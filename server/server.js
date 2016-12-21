e7c4307380fac5b79b61f0c51e72188e872d1702
Phil <plmok61@gmail.com>
2016-12-21 14:38:22 -0600

    sets up express and routes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
-var express = require('express')
-var app = express()
-var path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const router = require('./routes/routes')
 
+const app = express()
+const http = require('http').Server(app)
+const port = 3000
 
-app.get('/', function(req, res) {
-  res.sendFile(path.resolve(__dirname, '../client/index.html'))
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTION")
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
 })
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, '../client')))
+app.use('/', router)
 
-var port = 3000
-
-app.listen(port, function () {
-  console.log('Running on port ', port)
-})
+const server = http.listen(port)
+console.log(`Server is running on port ${port}`)
